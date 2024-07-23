f93ac4699a6aca28b77a169b571b5d466ea2e1d7
Elí Abdiel Chan López <157451029+EliAbdiel@users.noreply.github.com>
2024-07-23 14:46:52 -0500

    Update comandos-sql.sql

--- comandos-sql.sql
+++ comandos-sql.sql
@@ -16,4 +16,44 @@
   CREATE USER 'admin'@'localhost' IDENTIFIED BY 'password';
   GRANT ALL PRIVILEGES ON mydatabase.* TO 'admin'@'localhost';
   FLUSH PRIVILEGES;
+# Crear una tabla.
+  USE jugos;
+  CREATE TABLE TBCLIENTES (
+    DNI VARCHAR(20),
+    NOMBRE VARCHAR(150),
+    DIRECCION1 VARCHAR(150),
+    DIRECCION2 VARCHAR(150),
+    BARRIO VARCHAR(50),
+    CIUDAD VARCHAR(50),
+    ESTADO VARCHAR(50),
+    CP VARCHAR(10),
+    EDAD SMALLINT,
+    SEXO VARCHAR(1),
+    LIMITE_CREDITO FLOAT,
+    VOLUMEN_COMPRA FLOAT,
+    PRIMERA_COMPRA BIT
+    
+    );
+  CREATE TABLE tbproductos (
+    producto VARCHAR(20),
+    nombre VARCHAR(150),
+    envase VARCHAR(50),
+    volumen VARCHAR(20),
+    sabor VARCHAR(50),
+    precio FLOAT
+    
+  );
+  CREATE TABLE TABLA_DE_VENDEDORES (
+    MATRICULA varchar(5), 
+    NOMBRE varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci, 
+    PORCENTAJE_COMISION float
+    
+    );
+# Cambiar el tipo de dato de una columna.
+  ALTER TABLE nombre_tabla MODIFY nombre_columna INT;
+# Cambiar el tamaño de una columna.
+  ALTER TABLE nombre_tabla MODIFY nombre_columna VARCHAR(100);
+# Eliminar una tabla.
+  DROP TABLE TBCLIENTES;
 
+  
