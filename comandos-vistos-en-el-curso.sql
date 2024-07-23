2e433408e683af0e67cdd4e5601a2b95431d45ac
Elí Abdiel Chan López <157451029+EliAbdiel@users.noreply.github.com>
2024-07-23 13:06:16 -0500

    Update comandos-vistos-en-el-curso.sql

---
+++ comandos-vistos-en-el-curso.sql
@@ -0,0 +1,17 @@
+#Los comandos vistos en el aula anterior son los siguientes:
+
+USE world;
+
+SELECT * FROM city;
+
+SELECT * FROM country;
+
+SELECT * FROM countrylanguage;
+
+CREATE DATABASE jugos;
+
+DROP SCHEMA jugos;
+
+SELECT * FROM CITY;
+
+select * from country;
