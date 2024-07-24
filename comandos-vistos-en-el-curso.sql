882b1c2bef5608da5fcc6710a694055b08f06299
Elí Abdiel Chan López <157451029+EliAbdiel@users.noreply.github.com>
2024-07-23 23:47:52 -0500

    Update comandos-vistos-en-el-curso.sql

--- comandos-vistos-en-el-curso.sql
+++ comandos-vistos-en-el-curso.sql
@@ -16,7 +16,7 @@ SELECT * FROM CITY;
 
 select * from country;
 
-# Comandos vistos en el aula anterior Parte[2] son los siguientes:
+USE JUGOS;
 
 CREATE TABLE TBCLIENTES(
 DNI VARCHAR(20),
@@ -42,3 +42,111 @@ ENVASE VARCHAR (50) ,
 VOLUMEN VARCHAR (50) ,
 SABOR VARCHAR (50) ,
 PRECIO FLOAT);
+
+USE jugos;
+
+INSERT INTO tbproductos(
+producto, nombre, envase, volumen, sabor,
+precio) VALUES ('773912', 'clean', 'botella pet',
+'1 litro', 'naranja', 8.01);
+
+SELECT * FROM tbproductos;
+
+INSERT INTO tbproductos(
+producto, nombre, envase, volumen, sabor,
+precio) VALUES ('838819', 'clean', 'botella pet',
+'1.5 litros', 'naranja', 12.01);
+INSERT INTO tbproductos(
+producto, nombre, envase, volumen, sabor,
+precio) VALUES ('1037797', 'clean', 'botella pet',
+'2 litros', 'naranja', 16.01);
+INSERT INTO tbproductos(
+producto, nombre, envase, volumen, sabor,
+precio) VALUES ('8128292', 'clean', 'latat',
+'350 ml', 'naranja', 2.81);
+
+SELECT * FROM tbproductos;
+
+USE jugos;
+
+INSERT INTO tbproductos(
+producto, nombre, envase, volumen, sabor,
+precio) VALUES ('695594', 'Festival de Sabores', 'Botella PET',
+'1.5 Litros', 'Asaí', 18.51);
+
+INSERT INTO tbproductos(
+producto, nombre, envase, volumen, sabor,
+precio) VALUES ('1041119', 'Línea Citrus', 'Botella de Vidrio',
+'700 ml', 'Lima', 4.90);
+
+SELECT * FROM tbproductos;
+
+UPDATE tbproductos SET producto = '812829', envase = 'lata'
+WHERE volumen = '350 ml';
+
+UPDATE tbproductos SET precio = 28.51
+WHERE producto = '695594';
+
+UPDATE tbproductos SET sabor = 'Lima/Limón', precio = 4.90
+WHERE producto = '1041119';
+
+SELECT * FROM tbproductos;
+
+DELETE FROM tbproductos WHERE producto = '773912';
+
+INSERT INTO tbproductos(
+producto, nombre, envase, volumen, sabor,
+precio) VALUES ('773912', 'clean', 'botella pet',
+'1 litro', 'naranja', 8.01);
+
+SELECT * FROM tbproductos;
+
+ALTER TABLE tbproductos ADD PRIMARY KEY(PRODUCTO);
+
+
+USE jugos;
+
+ALTER TABLE tbclientes ADD PRIMARY KEY (DNI);
+
+ALTER TABLE tbclientes ADD COLUMN(FECHA_NACIMIENTO DATE);
+
+INSERT INTO tbclientes(
+DNI,
+NOMBRE,
+DIRECCION1, 
+DIRECCION2, 
+BARRIO,
+CIUDAD,
+ESTADO,
+CP,
+EDAD,
+SEXO, 
+LIMITE_CREDITO,
+VOLUMEN_COMPRA, 
+PRIMERA_COMPRA,
+FECHA_NACIMIENTO) VALUES(
+'456879548', 'Pedro el Escamoso', 'Calle del Sol, 25', '', 'Los Laureles', 'CDMX', 'México', '65784', 55, 'M',
+1000000, 2000, 0, '1971-05-25');
+
+SELECT * FROM tbclientes;
+
+DROP TABLE TABLA_DE_VENDEDORES;
+
+CREATE TABLE TABLA_DE_VENDEDORES(
+  MATRICULA varchar(5),
+  NOMBRE varchar(100),
+  PORCENTAJE_COMISION float,
+  FECHA_ADMISION date,
+  DE_VACACIONES bit(1)
+  );
+
+ALTER TABLE TABLA_DE_VENDEDORES ADD PRIMARY KEY (MATRICULA);
+
+INSERT INTO TABLA_DE_VENDEDORES
+(MATRICULA, NOMBRE, FECHA_ADMISION, PORCENTAJE_COMISION, DE_VACACIONES) VALUES ('00235','Márcio Almeida Silva','2014-08-15',0.08,0);
+
+INSERT INTO TABLA_DE_VENDEDORES
+(MATRICULA, NOMBRE, FECHA_ADMISION, PORCENTAJE_COMISION, DE_VACACIONES) VALUES ('00236','Cláudia Morais','2013-09-17',0.08,1);
+
+INSERT INTO TABLA_DE_VENDEDORES
+(MATRICULA, NOMBRE, FECHA_ADMISION, PORCENTAJE_COMISION, DE_VACACIONES) VALUES ('00238','Pericles Alves','2016-08-21',0.11,0);
